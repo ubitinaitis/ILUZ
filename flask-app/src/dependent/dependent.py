bf9d6aa972768700fc246c8f78fd372114697b39
Ignatius <iggym2002@gmail.com>
2022-12-13 18:46:30 -0500

    More routes

--- flask-app/src/dependent/dependent.py
+++ flask-app/src/dependent/dependent.py
@@ -21,6 +21,22 @@ def get_dependents(userID):
 
 # get request for budget
 
+@dep_blueprint.route('/dep/<userID>', methods=['GET'])
+def get_budget(userID):
+    cursor = db.get_db().cursor()
+
+    cursor.execute('SELECT b.category, bal.currentFunds FROM Dependent d JOIN Balance bal\
+     ON d.hasBalance = bal.balanceID JOIN Budget b ON b.budgetID = bal.balanceID\
+      WHERE d.userID = {0}'.format(userID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    data = cursor.fetchall()
+    for row in data:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
 
 # post requests to add funds to a particular budget for independent and dependent
 # (ex. post request for an independent to add that they just spent x dollars on groceries)
